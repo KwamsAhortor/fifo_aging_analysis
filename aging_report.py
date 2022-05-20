31a8effe0a1392a044a1c91d02b4c0818d3a69ea
Michael Faraday <mfaraday01@af.etcred.com>
2022-05-20 11:21:53 +0000

    Load data

--- aging_report.py
+++ aging_report.py
@@ -4,5 +4,12 @@ import numpy as np
 from datetime import datetime
 from datetime import timedelta
 
-
+# Load data
+xlsx = r"C:\Users\mfaraday01\Documents\Code\Aging Analysis\credit_debit_records.xlsx"
+debit = pd.read_excel (xlsx, 
+                     sheet_name="Debits"
+                     )
+credit = pd.read_excel (xlsx, 
+                     sheet_name="Credit"
+                     )
 
