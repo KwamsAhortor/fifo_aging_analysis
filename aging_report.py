cae7bfb3b4cf3f887a9be0063388345500c05040
Michael Faraday <mfaraday01@af.etcred.com>
2022-06-19 12:56:17 +0000

    Add column for overdue categories

--- aging_report.py
+++ aging_report.py
@@ -18,3 +18,32 @@ df = df.melt(id_vars=["Name"], var_name="Date", value_name="Amount")
 
 # Change datatypes
 df["Date"] = pd.to_datetime(df["Date"]).dt.date
+
+# Add new calculated columns
+
+df["Due date"] = df["Date"] + timedelta(days=7)
+df["Overdue"] = (datetime.date(datetime.now()) - df["Due date"]).dt.days.astype('int16')
+
+# Categorize overdue time to ages
+
+conditions = [
+               (df["Overdue"] <= 7),
+               (df["Overdue"] > 7) & (df["Overdue"] <= 14),
+               (df["Overdue"] > 14) & (df["Overdue"] <= 21),
+               (df["Overdue"] > 21) & (df["Overdue"] <= 60),
+               (df["Overdue"] > 60) & (df["Overdue"] <= 120),
+               (df["Overdue"] > 120) & (df["Overdue"] <= 180),
+               (df["Overdue"] > 180)               
+               ]
+
+choices = [
+            "1-7 days",
+            "8-15 days",
+            "16-21 days",
+            "22-60 days",
+            "61-120 days",
+            "121-180 days",
+            "Above 180"
+            ]
+
+df["Aging Status"] = np.select(conditions, choices)
